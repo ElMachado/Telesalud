71979911e7100fa07061bf2a223f182f99b60302
Jesus D Machado <elmachado@hotmail.es>
2019-09-14 23:03:28 -0500

    Se agregan los formularios de inicio de sesión y registro

---
+++ app/src/main/java/com/example/telesalud/Login.java
@@ -0,0 +1,22 @@
+package com.example.telesalud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+public void Registro(View view){
+        Intent Registro = new Intent(this,Registro.class);
+        super.startActivity(Registro);
+    }
+
+}
